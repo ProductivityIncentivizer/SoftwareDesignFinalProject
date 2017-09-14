6ded0f0065695c31a42db7fb3e90763fcbeea0ac
smittelstadt <sophie.mittelstadt@girlswhocode.com>
2017-09-14 15:36:56 -0600

    Added the 1st test for addAccount (& added another method in main class)

--- ProductivityIncentivizer1/src/ProductivityIncentivizer.java
+++ ProductivityIncentivizer1/src/ProductivityIncentivizer.java
@@ -12,7 +12,7 @@ public class ProductivityIncentivizer {
 	public void promptUserToCreateAccount(){
 		String employeeName;
 		String employeeType;
-		AccountFactory accountFactory = new AccountFactory();
+		String supervisorCode = "";
 		Scanner in = new Scanner(System.in);
 		
 		System.out.println("Please enter your name.");
@@ -21,27 +21,40 @@ public class ProductivityIncentivizer {
 		System.out.println("Please select student worker (1) or supervisor (2).");
 		employeeType = in.nextLine();
 		
-		if (employeeType.equals("1")){
-			accountFactory.getAccount(employeeType, employeeName);		
-		}
-
-		
-		else if (employeeType.equals("2")){
+		if(employeeType.equals("2")) {
 			boolean supervisorCodeCheck = false;
 			while(supervisorCodeCheck == false) {
-				
 				System.out.println("Please enter supervisor code: ");
-				String supervisorCode = in.nextLine();
-				if (supervisorCode.equals("123")) {
-					supervisorCodeCheck = true;
-					accountFactory.getAccount(employeeType, employeeName);
-				}
-				else {
-					System.out.println("Supervisor code was incorrect. Please try again.");
-				}
+				supervisorCode = in.nextLine();	
 			}
 		}
+		
 		in.close();
+		addAccount(employeeName, employeeType, supervisorCode);
+	}
+	
+	/**
+	 * Adds account, called by promptUserToCreateAccount
+	 * @param employeeName
+	 * @param employeeType
+	 * @return boolean used for testing
+	 */
+	public boolean addAccount(String employeeName, String employeeType, String supervisorCode) {
+		AccountFactory accountFactory = new AccountFactory();
+		if (employeeType.equals("1")) {
+			accountFactory.getAccount(employeeType, employeeName);		
+			return true;
+		}
+		else if (employeeType.equals("2")){
+			if (supervisorCode.equals("123")) {
+				accountFactory.getAccount(employeeType, employeeName);
+				return true;
+			}
+			else { //invalid code input
+					return false;
+			}
+		}
+		return false;
 	}
 
 	public static void main(String[] args) {
