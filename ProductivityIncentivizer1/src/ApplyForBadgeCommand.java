39f838aae7093b5f61ee57c30dbe49462eb5637e
palesa <palesa@172.17.65.173>
2017-09-14 16:53:31 -0600

    parameters to receive for badge application and method to change badge status

--- ProductivityIncentivizer1/src/ApplyForBadgeCommand.java
+++ ProductivityIncentivizer1/src/ApplyForBadgeCommand.java
@@ -1,13 +1,43 @@
+import java.util.ArrayList;
 
+/**
+ * this class defines the process for applying for a badge
+ * A student worker sends in a list of all the badges he/she is applying for 
+ * */
 public class ApplyForBadgeCommand implements BadgeCommand {
 
-	public ApplyForBadgeCommand() {
+	private ArrayList<Badge> badgeList;
+	private String studentWorkerName;
+	 private String status = "";
+	 private int iD;
+	// private ArrayList<Badge> ;
+	
+	public ApplyForBadgeCommand(String studentWorkerName,int iD, ArrayList<Badge> badgeList) {
+		this.badgeList = badgeList;
+		this.studentWorkerName = studentWorkerName;
+		this.iD = iD;
 		
 	}
 
+	
 	@Override
 	public void executeCommand() {
-		// TODO Auto-generated method stub
+		
+		for(Badge badges: badgeList) {
+			
+			if(badges!= null) {
+			   updateBadgeStatus(badges);
+				
+			   
+				//In process == status
+				//method that deals with database
+				
+			   //change applicant user ID at the same time
+				
+				
+			}
+			
+		}
 		
 	}
 
@@ -15,6 +45,23 @@ public class ApplyForBadgeCommand implements BadgeCommand {
 	public void undoCommnd() {
 		// TODO Auto-generated method stub
 		
+	}
+	
+	/**
+	 * method meant to simulate what changing a badge status would look like
+	 * @param badge is the badge received
+	 * */
+   public void updateBadgeStatus(Badge badge) {
+	   this.status = "In progress";
+	   
+	   
+	   
+	   
+   }
+	public String toString() {
+		
+		
+		return "";
 	}
 
 }
